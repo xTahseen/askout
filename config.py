f5489f9bcc910af2342403e8c630e75ce7977316
Syed Tahseen <syedtahseen38@gmail.com>
2025-07-09 01:30:09 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+# Toggle whether to generate an image for every anonymous message
+GENERATE_IMAGE_ON_ANONYMOUS = True
+
+# Toggle whether users can reply to anonymous messages
+ALLOW_ANONYMOUS_REPLY = True
